f27a1fb4b3411302b47cfbc23d75c9d4bf90584a
marcweal <m.weal03@googlemail.com>
2019-06-20 10:21:28 +0000

    added db support

--- app/Http/Controllers/V1/PlaqueController.php
+++ app/Http/Controllers/V1/PlaqueController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\V1;
 use App\Http\Controllers\Controller;
 use App\Models\Plaque;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PlaqueController extends Controller
 {
